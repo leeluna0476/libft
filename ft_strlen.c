8d56761cb9aafcc297d096ad525d1ef84f5e7abe
LEE LUNA <118426492+leeluna0476@users.noreply.github.com>
2024-09-13 09:33:12 +0900

    strlen: remove unnecessary comments

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,15 +3,11 @@
      size_t
      strlen(const char *s);
 
-     size_t
-     strnlen(const char *s, size_t maxlen);
-
 DESCRIPTION
-     The strlen() function computes the length of the string s.  The strnlen() function attempts to compute the length of s, but never scans beyond the first maxlen bytes of s.
+     The strlen() function computes the length of the string s.
 
 RETURN VALUES
-     The strlen() function returns the number of characters that precede the terminating NUL character.  The strnlen() function returns either the same result as strlen() or maxlen,
-     whichever is smaller.
+     The strlen() function returns the number of characters that precede the terminating NUL character.
 */
 
 # include "libft.h"
