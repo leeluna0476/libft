71434caa45f15306c6bc5c0491e649c3502cd272
LEE LUNA <118426492+leeluna0476@users.noreply.github.com>
2024-09-20 14:22:37 +0900

    strrchr: add the variable again

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -2,11 +2,12 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
+    char	chr;
 	size_t	i;
 	
 	chr = (char)c;
 	i = ft_strlen(s);
-	while (*(s + i) != c)
+	while (*(s + i) != chr)
 	{
 		if (!i)
 		{
