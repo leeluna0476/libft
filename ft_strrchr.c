a1d4c529c3f5e9b08aa5d80f043d61460d01f984
seojilee <seojilee@student.42seoul.kr>
2024-12-10 17:24:59 +0900

    norm

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seojilee <seojilee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 17:20:23 by seojilee          #+#    #+#             */
+/*   Updated: 2024/12/10 17:22:02 by seojilee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-    char	chr;
+	char	chr;
 	size_t	i;
-	
+
 	chr = (char)c;
 	i = ft_strlen(s);
 	while (*(s + i) != chr)
