ba2f1d065da6d5f572f6f1104eedb1c95cf20ac7
leeluna0476 <leeluna0476@daum.net>
2025-02-01 22:48:48 +0900

    fix email

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: seojilee <seojilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 17:20:08 by seojilee          #+#    #+#             */
-/*   Updated: 2024/12/11 19:15:45 by seojilee         ###   ########.fr       */
+/*   Updated: 2025/02/01 22:48:07 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ char	*ft_strchr(const char *s, int c)
 	while (*s != chr)
 	{
 		if (!*s)
-		{
 			return (NULL);
-		}
 		++s;
 	}
 	return ((char *)s);
