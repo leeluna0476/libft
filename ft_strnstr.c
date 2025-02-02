80655b45dcebc4d373b92a2127abdcc81251b856
LEE LUNA <seojilee@student.42seoul.kr>
2025-02-02 09:32:19 +0900

    unify style: remove brackets

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -20,9 +20,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (len >= needle_len)
 	{
 		if (ft_strncmp(haystack, needle, needle_len) == 0)
-		{
 			return ((char *)haystack);
-		}
 		++haystack;
 		--len;
 	}
