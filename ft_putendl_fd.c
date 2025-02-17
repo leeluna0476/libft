386f6ef028f12dd8857d4ea1d43c236f1027edb0
seojilee <seojilee@student.42seoul.kr>
2025-02-17 13:30:07 +0900

    check partial writes

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: seojilee <seojilee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:09:09 by seojilee          #+#    #+#             */
-/*   Updated: 2025/01/30 15:12:14 by seojilee         ###   ########.fr       */
+/*   Updated: 2025/02/17 13:29:48 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 /*
  * Partial and deferred writes는 O_NONBLOCK이 세팅되어 있을 때만 발생.
- * fcntl을 사용할 수 없기 때문에 확인할 방법 X.
+ * fcntl을 사용할 수 없기 때문에 따로 확인할 방법 X.
  * errno를 사용할 수 없고, void 반환형이기 때문에 예외처리를 할 방법 X.
- * => partial and deferred writes는 고려하지 않는다.
+ * partial writes는 반복해서 write하고, deferred writes는 무시한다.
  */
 void	ft_putendl_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
 }
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: seojilee <seojilee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:07:12 by seojilee          #+#    #+#             */
-/*   Updated: 2025/01/30 15:12:05 by seojilee         ###   ########.fr       */
+/*   Updated: 2025/02/17 13:29:36 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,20 @@
 
 /*
  * Partial and deferred writes는 O_NONBLOCK이 세팅되어 있을 때만 발생.
- * fcntl을 사용할 수 없기 때문에 확인할 방법 X.
+ * fcntl을 사용할 수 없기 때문에 따로 확인할 방법 X.
  * errno를 사용할 수 없고, void 반환형이기 때문에 예외처리를 할 방법 X.
- * => partial and deferred writes는 고려하지 않는다.
+ * partial writes는 반복해서 write하고, deferred writes는 무시한다.
  */
 void	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	size_t	written;
+	size_t	to_write;
+
+	written = 0;
+	to_write = ft_strlen(s);
+	while (to_write)
+	{
+		written = write(fd, s + written, to_write);
+		to_write -= written;
+	}
 }
