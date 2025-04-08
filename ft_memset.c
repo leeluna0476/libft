42f5025de209fbd1993bea18b02198e69cc20d00
LEE LUNA <seojilee@student.42seoul.kr>
2025-04-09 00:41:16 +0900

    memset 최적화 시도 1: 8바이트씩 채우기

--- ft_memset.c
+++ ft_memset.c
@@ -6,22 +6,36 @@
 /*   By: seojilee <seojilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 17:20:04 by seojilee          #+#    #+#             */
-/*   Updated: 2024/12/11 19:11:48 by seojilee         ###   ########.fr       */
+/*   Updated: 2025/04/09 00:40:53 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// c표준: memory size_t로 정렬 필요.
+// 아래는 임시...
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*t;
+	unsigned char	*ptr;
+	size_t			wordsize_c;
+	size_t			divlen_size;
+	size_t			modlen_size;
 
-	t = (unsigned char *)b;
-	while (len > 0)
+	ptr = (unsigned char *)b;
+	wordsize_c = c;
+	for (int i = 1; i < 8; ++i)
+		wordsize_c |= wordsize_c << (i * sizeof(size_t));
+	divlen_size = len / sizeof(size_t);
+	while (divlen_size-- > 0)
 	{
-		*t = c;
-		++t;
-		--len;
+		*(size_t *)ptr = wordsize_c;
+		ptr += sizeof(size_t);
+	}
+	modlen_size = len % sizeof(size_t);
+	while (modlen_size-- > 0)
+	{
+		*ptr = c;
+		++ptr;
 	}
 	return (b);
 }
