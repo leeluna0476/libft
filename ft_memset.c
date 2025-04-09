a3a527025bbeb5702694bb0f36448036e722b377
LEE LUNA <seojilee@student.42seoul.kr>
2025-04-09 11:10:34 +0900

    memset 최적화: 메모리 정렬 완

--- ft_memset.c
+++ ft_memset.c
@@ -6,36 +6,74 @@
 /*   By: seojilee <seojilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 17:20:04 by seojilee          #+#    #+#             */
-/*   Updated: 2025/04/09 00:40:53 by seojilee         ###   ########.fr       */
+/*   Updated: 2025/04/09 11:09:47 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// c표준: memory size_t로 정렬 필요.
-// 아래는 임시...
+static uintptr_t	align_ptr(void *b, size_t align_size)
+{
+	uintptr_t	uint_ptr;
+	uintptr_t	if_not_aligned;
+
+	uint_ptr = (uintptr_t)b;
+	if_not_aligned = uint_ptr % align_size;
+	if (if_not_aligned)
+		return (uint_ptr + (align_size - if_not_aligned));
+	else
+		return (uint_ptr);
+}
+
+static void	fill_in_buf_by_size(void *p, size_t c, size_t len, size_t type_size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (type_size == sizeof(size_t))
+			((size_t *)p)[i] = c;
+		else
+			((unsigned char *)p)[i] = c;
+		++i;
+	}
+}
+
+static size_t	expand_1_to_word(char c)
+{
+	size_t	wordsize_c;
+	size_t	i;
+
+	wordsize_c = c;
+	i = 1;
+	while (i < sizeof(size_t))
+	{
+		wordsize_c |= wordsize_c << (i * 8);
+		i <<= 1;
+	}
+	return (wordsize_c);
+}
+
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*ptr;
+	uintptr_t		aligned_ptr;
+	size_t			pre_align_bytes;
 	size_t			wordsize_c;
 	size_t			divlen_size;
 	size_t			modlen_size;
 
-	ptr = (unsigned char *)b;
-	wordsize_c = c;
-	for (int i = 1; i < 8; ++i)
-		wordsize_c |= wordsize_c << (i * sizeof(size_t));
+	aligned_ptr = align_ptr(b, sizeof(size_t));
+	pre_align_bytes = aligned_ptr - (uintptr_t)b;
+	fill_in_buf_by_size(b, c, pre_align_bytes, sizeof(char));
+	len -= pre_align_bytes;
+	wordsize_c = expand_1_to_word(c);
 	divlen_size = len / sizeof(size_t);
-	while (divlen_size-- > 0)
-	{
-		*(size_t *)ptr = wordsize_c;
-		ptr += sizeof(size_t);
-	}
+	fill_in_buf_by_size(\
+			(void *)aligned_ptr, wordsize_c, divlen_size, sizeof(size_t));
 	modlen_size = len % sizeof(size_t);
-	while (modlen_size-- > 0)
-	{
-		*ptr = c;
-		++ptr;
-	}
+	fill_in_buf_by_size(\
+			(void *)(aligned_ptr + divlen_size * sizeof(size_t)), \
+			c, modlen_size, sizeof(unsigned char));
 	return (b);
 }
